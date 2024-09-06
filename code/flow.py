aada3c9348c2f449831d783fec6d663ed95e0ca7
Darth Roman <chouchane.louai20@gmail.com>
2024-09-06 22:27:09 +0200

    Flow control session

---
+++ code/flow.py
@@ -0,0 +1,57 @@
+'''
+    # Flow control is used to control how your program can act in diffrent situations of diffrent scenarios
+    and the logic of your code
+    # if statments, if..else etc...
+'''
+
+# Examples
+# age = 18
+# if False:
+#     print("Congrats, you are legally and adult")
+# else:
+#     print("You are still a baby")
+
+# # in statement (List)
+# students = ["Louai", "Maria", "Marie", "Meriem", "Aya"]
+# print("Sara" in students)
+
+# Practice
+
+##### Loops #####
+## They are used to repeat a routine for a specific amount of repeats or until a condition is 
+## either True or False
+
+## For loops
+
+# for i in range(10):
+#     print(f"Hello {i}")
+
+
+students = ["Louai", "Maria", "Marie", "Meriem", "Aya"]
+# for i in students:
+#     if i == "Louai":
+#         print("Male")
+#     else:
+#         print("Female")
+
+first_student = students[0]
+
+# i = len(students)-1
+# while first_student != students[i]:
+#     print(students[i])
+#     i-=1
+
+# age = 5
+# while age <= 18:
+#     print("You are an adult")
+#     age += 1
+
+
+
+
+
+
+
+
+
+
